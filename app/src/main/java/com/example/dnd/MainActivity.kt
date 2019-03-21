7c601f31cdcd21486348131168715087237f7c32
Diego <reloth94@hotmail.com>
2019-03-21 19:31:20 +0100

    first view with all skills

--- app/src/main/java/com/example/dnd/MainActivity.kt
+++ app/src/main/java/com/example/dnd/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.dnd
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.math.floor
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun calcMod(x: Int): Int{
+        return floor((x.toDouble()-10)/2).toInt()
     }
 }
