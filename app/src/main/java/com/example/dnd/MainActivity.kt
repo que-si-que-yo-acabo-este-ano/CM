c09d93e34f963f5c7e7df9e17fd7cb3a6d874f1b
Diego <reloth94@hotmail.com>
2019-03-22 17:09:40 +0100

    calculo mod str done

--- app/src/main/java/com/example/dnd/MainActivity.kt
+++ app/src/main/java/com/example/dnd/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.dnd
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.floor
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +12,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        editText2.addTextChangedListener(
+            object : TextWatcher {
+
+                override fun afterTextChanged(s: Editable) {}
+
+                override fun beforeTextChanged(
+                    s: CharSequence, start: Int,
+                    count: Int, after: Int
+                ) {
+                }
+
+                override fun onTextChanged(
+                    s: CharSequence, start: Int,
+                    before: Int, count: Int
+                ) {
+                    var stat: Int
+
+                    if (s.any()){
+                        stat = Integer . parseInt (s.toString())
+                    }else{
+                        stat = 0
+                    }
+
+                    if (stat < 10) {
+                        textView8.setText(calcMod(stat))
+                    }else{
+                        textView8.setText("+" + calcMod(stat))
+                    }
+                }
+            })
     }
 
 
-    fun calcMod(x: Int): Int{
-        return floor((x.toDouble()-10)/2).toInt()
+
+
+
+    fun calcMod(x: Int): String{
+        return floor((x.toDouble()-10)/2).toString()
     }
 }
